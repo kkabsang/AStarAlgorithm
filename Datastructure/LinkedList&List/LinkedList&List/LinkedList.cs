e3f6b5894ab4a82373af564a8f28136700a8cf28
leedasom <65thawkd@naver.com>
2022-09-30 18:21:19 +0900

    3

---
+++ Datastructure/LinkedList&List/LinkedList&List/LinkedList.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithim1
+{
+    class MyLinkedListNode<T>
+    {
+        public T Data;
+        public MyLinkedListNode<T> Next;//다음방
+        public MyLinkedListNode<T> Prev;//이전방
+    }
+    class MyLinkedList<T>
+    {
+        public MyLinkedListNode<T> Head = null;
+        public MyLinkedListNode<T> Tail = null;
+        public int Count = 0;
+
+        //O(1)
+        public MyLinkedListNode<T> AddLast(T data)
+        {
+            MyLinkedListNode<T> newRoom = new MyLinkedListNode<T>();
+            newRoom.Data = data;
+
+            //방이 없을때 새로 추가한 방이 head
+            if (Head == null)
+            { Head = newRoom; }
+            //마지막 방과 새로 추가되는 방을 연결 
+            if (Tail != null)
+            {
+                Tail.Next = newRoom;
+                newRoom.Prev = Tail;
+            }
+            //새로추가된 방  tail
+            Tail = newRoom;
+            Count++;
+            return newRoom;
+        }
+        //O(1)
+        public void Remove(MyLinkedListNode<T> room)
+        {
+            //첫번째 다음방을 첫번째방으로 
+            if (Head == room)
+                Head = Head.Next;
+            //마지막방 이전방을 마지막방으로 
+            if (Tail == room)
+                Tail = Tail.Prev;
+            if (room.Prev != null)
+                room.Prev.Next = room.Next;
+            if (room.Next != null)
+                room.Next.Prev = room.Prev;
+            Count--;
+        }
+    }
+    class Board
+    {
+        public int[] _data = new int[25];
+
+        public MyLinkedList<int> _data3 = new MyLinkedList<int>();//연결리스트
+        public void Initialize()
+        {
+            _data3.AddLast(101);
+            _data3.AddLast(102);
+            MyLinkedListNode<int> node = _data3.AddLast(103);
+            _data3.AddLast(104);
+            _data3.AddLast(105);
+
+            _data3.Remove(node);
+        }
+    }
+}
---
+++ Datastructure/LinkedList&List/LinkedList&List/List.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithim
+{
+    class MyList<T>
+    {
+        const int DEFAULT_SIZE = 1;
+        T[] _data = new T[DEFAULT_SIZE];
+        public int Count = 0;//실제로 사용중인 데이터 개수
+        public int Capacity { get { return _data.Length; } }//예약된 데이터 개수
+
+        //O(1) 예외케이스-이사비용 고려 x
+        public void Add(T item)
+        {
+            //공간이 남았는지?
+            if (Count >= Capacity)
+            {
+                //공간 늘리기
+                T[] newArray = new T[Count * 2];
+                for (int i = 0; i < Count; i++)
+                {
+                    newArray[i] = _data[i];
+                }
+                _data = newArray;
+            }
+            //공간에 데이터 삽입
+            _data[Count] = item;
+            Count++;
+        }
+        //O(1)
+        public T this[int index]
+        {
+            get { return _data[index]; }
+            set { _data[index] = value; }
+        }
+        //O(N)
+        public void RemoveAt(int index)
+        {
+            //
+            for (int i = index; i < Count - 1; i++)
+            {
+                _data[i] = _data[i + 1];
+            }
+            _data[Count - 1] = default(T);//마지막 방 삭제
+            Count--;
+        }
+    }
+    class Board
+    {
+        public int[] _data = new int[25];
+        public MyList<int> _data2 = new MyList<int>();//백터, 동적배열
+        public LinkedList<int> _data3 = new LinkedList<int>();//연결리스트
+        public void Initialize()
+        {
+            _data2.Add(101);
+            _data2.Add(102);
+            _data2.Add(103);
+            _data2.Add(104);
+            _data2.Add(105);
+
+            int temp = _data2[2];
+
+            _data2.RemoveAt(2);
+        }
+    }
+}
+//배열-연속된방 삽입,삭제x,  동적배열-삽입삭제O,N번째 접근 쉬움, 연결리스트-추가삭제 자유로움 N번째 접근힘듬 
+//시간복잡도
